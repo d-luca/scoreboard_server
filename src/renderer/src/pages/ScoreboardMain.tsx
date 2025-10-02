32088c7bab7f4bd318b121a74f3589bd4f02f11d
Luca Davi <l.davi@reply.it>
2025-10-02 08:58:48 +0200

    feat: add initial ssr scoreboard update to align it to the initial scoreboard store state

--- src/renderer/src/pages/ScoreboardMain.tsx
+++ src/renderer/src/pages/ScoreboardMain.tsx
@@ -1,10 +1,36 @@
 import { ScoreboardControl } from "@renderer/components/ScoreboardControl/ScoreboardControl";
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import { ScoreboardFeedback } from "@renderer/components/ScoreboardFeedback/ScoreboardFeedback";
 import { CameraFeedback } from "@renderer/components/CameraFeedback/CameraFeedback";
 import { ScoreboardSettings } from "@renderer/components/ScoreboardSettings";
+import { useScoreboardStore } from "@renderer/stores/scoreboardStore";
 
 export function ScoreboardMain(): JSX.Element {
+	const store = useScoreboardStore();
+	useEffect(() => {
+		window.api.updateScoreboardData({
+			half: store.half,
+			timer: store.timer,
+			teamHomeName: store.teamHomeName,
+			teamAwayName: store.teamAwayName,
+			teamHomeScore: store.teamHomeScore,
+			teamAwayScore: store.teamAwayScore,
+			teamHomeColor: store.teamHomeColor,
+			teamAwayColor: store.teamAwayColor,
+			eventLogo: store.eventLogo,
+		});
+	}, [
+		store.eventLogo,
+		store.half,
+		store.teamAwayColor,
+		store.teamAwayName,
+		store.teamAwayScore,
+		store.teamHomeColor,
+		store.teamHomeName,
+		store.teamHomeScore,
+		store.timer,
+	]);
+
 	return (
 		<div className="flex size-full gap-4">
 			<div className="flex size-full flex-col gap-4">
